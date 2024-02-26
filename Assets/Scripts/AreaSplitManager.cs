8e78e5ade1965ce357bd86644d946e65b7b1709a
LilKinny <kohlitegd@gmail.com>
2024-02-26 13:54:44 +0800

    File structure and culprit spawner refactoring

---
+++ Assets/Scripts/AreaSplitManager.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaSplitManager : MonoBehaviour
+{
+    public GameObject CulpritPrefab;
+    public int NumberOfCulprits = 5;
+    public float DeadzoneRadius = 0.5f;
+    private List<GameObject> SpawnedCulprits = new();
+    public int Floor = 0;
+    public CulpritsManager culpritManager;
+    public void RespawnCulprits()
+    {
+        // if (!mainGameManager) mainGameManager = MainGameManager.instance;
+        Vector3 boxSize = GetComponent<Renderer>().bounds.size;
+        float areaWidth = boxSize.x / NumberOfCulprits;
+        int s = 1;
+        for (int i = 0; i < NumberOfCulprits; i++)
+        {
+
+            float spawnPositionX = transform.position.x - (boxSize.x / 2) + (areaWidth * i) + (areaWidth / 2);
+            Vector3 spawnPosition = new Vector3(spawnPositionX, transform.position.y, transform.position.z);
+
+            if (!CheckCollision(spawnPosition))
+            {
+                GameObject GO = Instantiate(CulpritPrefab, spawnPosition, Quaternion.identity);
+                culpritManager.AddCulprit(GO.GetComponent<Culprit>());
+                GO.name = "Culprit " + culpritManager.SpawnedCulprits.Count;
+                Culprit culprit = GO.GetComponent<Culprit>();
+                culprit.column = s;
+                culprit.floor = Floor;
+                s++;
+              //  mainGameManager.CulpritPositions.Add(culprit.ShootPosition.position);
+            }
+        }
+    }
+    bool CheckCollision(Vector3 position)
+    {
+        Collider[] colliders = Physics.OverlapSphere(position, DeadzoneRadius); // Adjust the radius as needed
+
+        return colliders.Length > 0;
+    }
+
+
+    // DEPRECIATED
+    //public int numberOfAreas = 5; // Set the number of equally distanced areas here
+    //[Tooltip("How far the area has to be from a collidable object to actually spawn properly")]
+    //public float DeadzoneRadius = 0.5f;
+    //public GameObject objectToSpawn; // The object you want to spawn
+    //public MainGameManager mainGameManager;
+    //public int row;
+    //private void Start()
+    //{
+    //    SpawnCulprits();
+    //}
+    //bool CheckCollision(Vector3 position)
+    //{
+    //    Collider[] colliders = Physics.OverlapSphere(position, DeadzoneRadius); // Adjust the radius as needed
+
+    //    return colliders.Length > 0;
+    //}
+
+    //public void SpawnCulprits()
+    //{
+    //    if (!mainGameManager) mainGameManager = MainGameManager.instance;
+    //    Vector3 boxSize = GetComponent<Renderer>().bounds.size;
+    //    float areaWidth = boxSize.x / numberOfAreas;
+    //    int s = 1;
+    //    for (int i = 0; i < numberOfAreas; i++)
+    //    {
+
+    //        float spawnPositionX = transform.position.x - (boxSize.x / 2) + (areaWidth * i) + (areaWidth / 2);
+    //        Vector3 spawnPosition = new Vector3(spawnPositionX, transform.position.y, transform.position.z);
+
+    //        if (!CheckCollision(spawnPosition))
+    //        {
+    //            GameObject GO =Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
+    //            mainGameManager.SpawnedCulprits.Add(GO);
+    //            GO.name = "Culprit" + mainGameManager.SpawnedCulprits.Count;
+    //            Culprit culprit = GO.GetComponent<Culprit>();
+    //            culprit.column = s;
+    //            culprit.row = row;
+    //            s++;
+    //            mainGameManager.CulpritPositions.Add(culprit.ShootPosition.position);
+    //        }
+    //    }
+    //}
+}
---
+++ Assets/Scripts/CulpritsManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CulpritsManager : MonoBehaviour
+{
+    public List<Culprit> SpawnedCulprits = new();
+
+    public AreaSplitManager[] Floors;
+
+    public GameObject CulpritPrefab;
+
+    public int NumCulpritsPerRow;
+
+    private void Start()
+    {
+        InitFloors();
+    }
+
+    private void InitFloors()
+    {
+        Floors = GameObject.FindObjectsOfType<AreaSplitManager>();
+        for (int i = 0; i < Floors.Length; i++)
+        {
+            Floors[i].Floor = Floors.Length - i;
+            Floors[i].CulpritPrefab = CulpritPrefab;
+            Floors[i].culpritManager = this;
+            Floors[i].NumberOfCulprits = NumCulpritsPerRow;
+            Floors[i].RespawnCulprits();
+        }
+    }
+
+    public void ClearCulprits()
+    {
+        foreach(Culprit c in SpawnedCulprits)
+        {
+            Destroy(c.gameObject);
+        }
+    }
+
+    public void AddCulprit(Culprit C)
+    {
+        SpawnedCulprits.Add(C);
+    }
+}
