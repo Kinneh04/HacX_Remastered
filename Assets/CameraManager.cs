2429c36221d2fba4ce71755127c038472ac6b5af
JONATHAN ONG <211788R@mymail.nyp.edu.sg>
2024-03-06 10:06:28 +0800

    removed camera orbit script

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -170,10 +170,17 @@ public class CameraManager : MonoBehaviour
 
             HandleMovement();
             HandleScroll();
+
             if (Input.GetKeyDown(KeyCode.E))
             {
                 SwitchToFreecam(false);
             }
+        }
+
+        if(WindowsManager.Instance.isPrecisionMode)
+        {
+            if (Input.GetMouseButtonDown(2))
+                SwitchToFreecam(!freecamMode);
         }
 
         if (Input.GetKeyDown(KeyCode.Y))
@@ -212,6 +219,56 @@ public class CameraManager : MonoBehaviour
         float scrollAmount = scrollDelta * scrollSpeed * Time.deltaTime;
         moveSpeed += scrollAmount;
         moveSpeed = Mathf.Clamp(moveSpeed, 1f, 20f); // Adjust the min and max speed as needed
+    }
+
+    public void HandlePrecisionMovement()
+    {
+        //if (!WindowsManager.Instance.isPrecisionMode)
+        //    return;
+
+        //if (Input.GetMouseButtonDown(1)) // Check for right mouse button press
+        //{
+        //    isDragging = true;
+
+        //    if (vcam.Follow)
+        //        vcam.Follow = null;
+
+        //    lastMousePosition = Input.mousePosition;
+        //}
+        //else if (Input.GetMouseButtonUp(1)) // Check for right mouse button release
+        //{
+        //    isDragging = false;
+        //}
+
+        //if (Input.GetMouseButtonDown(2)) // Check for middle mouse button click
+        //{
+        //    Camera.main.orthographic = !Camera.main.orthographic; // Toggle orthographic view
+        //}
+
+
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");
+        ////float forwardInput = Input.GetAxis("Forward");
+
+        //transform.Translate(Vector3.right * horizontalInput * movementSpeed * Time.deltaTime);
+        //transform.Translate(Vector3.forward * verticalInput * movementSpeed * Time.deltaTime);
+        ////transform.Translate(transform.up * verticalInput * movementSpeed * Time.deltaTime);
+
+        //if (isDragging)
+        //{
+        //    Vector3 deltaMousePosition = Input.mousePosition - lastMousePosition;
+        //    lastMousePosition = Input.mousePosition;
+
+        //    // Rotate the camera around its own axes, simulating free rotation
+        //    transform.Rotate(Vector3.up, deltaMousePosition.x * rotationSpeed * Time.deltaTime);
+        //    transform.Rotate(Vector3.right, -deltaMousePosition.y * rotationSpeed * Time.deltaTime);
+        //    Vector3 currentRotation = transform.localEulerAngles;
+        //    currentRotation.z = 0;
+        //    transform.localEulerAngles = currentRotation;
+        //    // Optional: Add zoom functionality (scroll wheel)
+        //    float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+        //    transform.Translate(Vector3.forward * scrollWheel * 10 * Time.deltaTime);
+        //}
     }
 }
 
