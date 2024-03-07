7e1210180883c3abfcc9826bbef814898864c664
Koh Win <kohlitegd@gmail.com>
2024-03-07 12:12:19 +0800

    funny version controller

---
+++ Assets/VersionController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class VersionController : MonoBehaviour
+{
+    public TMP_Text VersionText;
+
+    public float currentVersion;
+
+    public void IncrementCurrentVersion()
+    {
+        currentVersion += 1;
+        VersionText.text = currentVersion.ToString();
+    }
+
+    private void Start()
+    {
+        VersionText.text = currentVersion.ToString();
+    }
+}
+
+[CustomEditor(typeof(VersionController))]
+public class VersionControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        VersionController myScript = (VersionController)target;
+
+        if (GUILayout.Button("Increment Version"))
+        {
+            myScript.IncrementCurrentVersion();
+        }
+    }
+}
+
+
+//[InitializeOnLoad]
+//public class SceneSaveCallback : MonoBehaviour
+//{
+//    static SceneSaveCallback()
+//    {
+       
+//    }
+
+//    private static void OnPlayModeStateChanged(PlayModeStateChange state)
+//    {
+//        if (state == PlayModeStateChange.ExitingEditMode || state == PlayModeStateChange.EnteredEditMode)
+//        {
+//            // This code will be executed when entering or exiting Play mode.
+//            Debug.Log("Play mode state changed.");
+//        }
+//    }
+
+//    private static void OnHierarchyChanged()
+//    {
+//        // This code will be executed when the hierarchy changes.
+//        Debug.Log("Hierarchy changed.");
+//    }
+
+//    private static void OnScriptsReloaded()
+//    {
+//        // This code will be executed when scripts finish reloading.
+//        Debug.Log("Scripts reloaded.");
+//    }
+//}
