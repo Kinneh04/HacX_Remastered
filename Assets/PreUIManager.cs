845346aaa40a573b277f9f322e938d76e903ff27
Koh Win <kohlitegd@gmail.com>
2024-03-14 13:02:47 +0800

    whoops

--- Assets/PreUIManager.cs
+++ Assets/PreUIManager.cs
@@ -8,12 +8,13 @@ public class PreUIManager : MonoBehaviour
     public Slider ChangeWindwCountSlider;
 
     public List<ModularHDB> ModHDBs = new();
-
+    public TMP_Text ChangeWindowAmountText;
     public void OnChangeWindowCount()
     {
         foreach(ModularHDB M in ModHDBs)
         {
             M.RefreshAllWindowCount((int)ChangeWindwCountSlider.value);
         }
+        ChangeWindowAmountText.text = ChangeWindwCountSlider.value.ToString("F0");
     }
 }
